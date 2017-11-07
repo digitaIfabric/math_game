d9e023dbb0e694d00621ffe8cd55468bb12d86f2
digitalfabric92 <dwawryko@gmail.com>
2017-11-07 01:33:53 +0000

    Initial commit

---
+++ game.rb
@@ -0,0 +1,37 @@
+# Use gets.chomp to get input from users and puts for output
+# Isolate your I/O logic to one or two classes. Don't allow all your classes to interact (gets/puts) with the User
+# Use puts, raise some_val.inspect, and byebug to help with debugging
+# What does #{rand_num[0]} plus #{rand_num[1]} equal?
+# P1 #{livesP1}/3 vs P2 #{livesP2}/3
+
+
+# Initialize new game
+# Initialize round 1
+# 2 instances of the player class @instances
+# Check if game over
+
+# Definition of new turn
+# Print ----- NEW TURN -----
+
+
+# Check game over (player lives >0)
+def game_over?
+
+# Print ----- GAME OVER -----
+end
+
+
+
+# Set the message that displays winner
+def winner_message
+
+end
+
+
+
+
+
+
+
+
+
---
+++ main.rb
@@ -0,0 +1,6 @@
+require "./game.rb"
+require "./player.rb"
+require "./round.rb"
+
+MathGame::Game.new
+
